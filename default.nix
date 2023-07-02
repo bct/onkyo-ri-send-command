5235a5ae50ba888176e074b49dba137038659e78
Brendan Taylor <bct@diffeq.com>
2023-07-02 08:25:55 -0600

    nix install works

--- default.nix
+++ default.nix
@@ -2,7 +2,15 @@
 
 pkgs.stdenv.mkDerivation {
   name = "onkyo-ri-send-command";
+  src = ./.;
+
   buildInputs = with pkgs; [
     (callPackage ./liblgpio.nix { })
   ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+
+    install -m 0755 onkyo-send-command $out/bin
+  '';
 }
